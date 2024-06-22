6a2d777fe3c59a871fa77a37069e739a658b3f88
pahu2353 <pahu2353@gmail.com>
2024-06-21 23:20:13 -0400

    Establish app flow with Stack Navigator, Tab Navigator, and Screens

--- app/index.tsx
+++ app/index.tsx
@@ -1,29 +1,33 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons'; // Correct import for Ionicons
+import { createStackNavigator } from '@react-navigation/stack';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
+// Tab navigation screens
 import Home from './screens/Home';
 import Explore from './screens/Explore';
 import Collection from './screens/Collection';
 
-const Tab = createBottomTabNavigator();
+// Stack navigation screens
+import Login from './screens/Login';
+import SignUp from './screens/SignUp';
 
-export default function Index() {
+
+// Creating instances of the navigators
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+const BottomTabNavigator = () => {
   return (
-    <Tab.Navigator
-      screenOptions={{ 
-        headerShown: false,
-        tabBarStyle: {
-          marginTop: 15,
-          borderTopWidth: 0, // Remove the top border
-        } 
-      }}
-    >
+
+    // Tab.screen is each "page" of the app. We wrap it in the Tab navigator because the navigation at the bottom is present in each main screen.
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
       <Tab.Screen
         name="Home"
         component={Home}
         options={{
-          tabBarIcon: ({ focused, color, size }) => (
+          tabBarIcon: ({ focused, color, size }) => ( // Using () instead of {} is a shorthand for "return." i.e., {} would require a return statement.
             <Ionicons
               name={focused ? 'home' : 'home-outline'}
               size={size}
@@ -51,7 +55,7 @@ export default function Index() {
         options={{
           tabBarIcon: ({ focused, color, size }) => (
             <Ionicons
-              name={focused ? 'albums' : 'albums-outline'}
+              name={focused ? 'albums' : 'album-outline'}
               size={size}
               color={color}
             />
@@ -59,5 +63,34 @@ export default function Index() {
         }}
       />
     </Tab.Navigator>
+  );
+};
+
+
+
+// Stack navigator pushes and pops screen, making them look like a single screen. 
+// We can see all the different pages in the Tab navigator, as you click from one to the other in the navbar.
+export default function Index() {
+  return (
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen
+          name="Login"
+          component={Login}
+        />
+        <Stack.Screen
+          name="TabNavigator"
+          component={BottomTabNavigator}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+        />
+
+      </Stack.Navigator>
   );
 }
