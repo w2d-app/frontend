c8245125b8d40d448a05d334eb0c489eeb52bfb7
Brian Zhang <brian.w.zhang@gmail.com>
2024-06-23 23:16:02 -0400

    added collections page with components for header and playlist items

--- app/screens/Collection.tsx
+++ app/screens/Collection.tsx
@@ -1,11 +1,55 @@
-// screens/CollectionScreen.tsx
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, FlatList, StyleSheet } from 'react-native';
+import PlaylistItem from '../components/PlaylistItem';
+import CollectionHeader from '../components/CollectionHeader'; // Import the CollectionHeader component
+
+const playlists = [
+  {
+    image: require('../../assets/images/cat_cover.jpg'),
+    name: 'Saved Activities',
+    description: 'TravelList 22 places',
+  },
+  {
+    image: require('../../assets/images/cat_cover.jpg'),
+    name: 'Waterloo',
+    description: 'Ontario, Canada',
+  },
+  {
+    image: require('../../assets/images/cat_cover.jpg'),
+    name: 'Toronto',
+    description: 'Ontario, Canada',
+  },
+  {
+    image: require('../../assets/images/cat_cover.jpg'),
+    name: 'Clubs',
+    description: 'Collection - By Dora',
+  },
+  {
+    image: require('../../assets/images/cat_cover.jpg'),
+    name: 'Yummy Food',
+    description: 'Collection - By Dora',
+  },
+];
 
 const Collection: React.FC = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Collection Screen</Text>
+    <View style={{ flex: 1 }}>
+      {/* Collection header */}
+      <CollectionHeader
+        profileImage={require('../../assets/images/cat_cover.jpg')}
+      />
+      {/* Playlist grid */}
+      <FlatList
+        data={playlists}
+        renderItem={({ item }) => (
+          <PlaylistItem
+            image={item.image}
+            name={item.name}
+            description={item.description}
+          />
+        )}
+        keyExtractor={(item) => item.name}
+      />
     </View>
   );
 };
